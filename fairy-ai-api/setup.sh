cff6cbde05fbff9d2992a8194a9d6786598a336a
Enzo Vandepoele <evandepoele@O03.fr>
2025-08-05 14:06:00 +0200

    Upd executable installation prérequis automatique

--- fairy-ai-api/setup.sh
+++ fairy-ai-api/setup.sh
@@ -12,7 +12,3 @@ pip install -r requirements.txt
 
 echo ">>> Lancement du serveur FastAPI..."
 uvicorn api:app --reload
-
-curl -X POST http://127.0.0.1:8000/ask \
--H "Content-Type: application/json" \
--d "{\"question\": \"Who is the youngest?\", \"csv_data\": \"Name,Age\nAlice,30\nBob,25\"}"
